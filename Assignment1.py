76e333ec76f75dc3373312e92232a1f6c955ec3a
Munyekiliz <elizakagwima@gmail.com>
2021-04-19 07:49:49 -0400

    Asst1

---
+++ Assignment1.py
@@ -0,0 +1,9 @@
+names = input("Enter your first and last name: ")
+#print(names)
+print("Name: " + " " + names)
+print()
+
+# Enter What are your goals for this class - long term goals?
+goals = input('What would you like to be ....when all is said and done with the class?: ')
+print("My goals are to be a : " + " " + goals)
+print()
